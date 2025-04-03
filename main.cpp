65205d704d83fc85f0f85405dafe1b0b8af58d67
Joeyboio <139588058+Joeyboio@users.noreply.github.com>
2025-04-03 10:37:21 -0400

    Clarifying "working statements"

--- main.cpp
+++ main.cpp
@@ -149,7 +149,7 @@ void initialize() {
 	pros::lcd::register_btn1_cb(on_center_button);
 
 
-    pros::lcd::set_text(4,"Selected auton:");
+    pros::lcd::set_text(4,"Selected Auton:");
 
     while(true){
         if (Button.get_new_press()) {
@@ -191,7 +191,7 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
-    pros::lcd::set_text(1, "working");
+    pros::lcd::set_text(1, "working Auton");
 }
 
 /**
@@ -211,7 +211,7 @@ void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-    pros::lcd::set_text(1, "working");
+    pros::lcd::set_text(1, "working driver control");
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
@@ -223,7 +223,7 @@ void opcontrol() {
         int leftY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         int rightX = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
-        //Move the robot
+        // move the robot
         chassis.arcade(leftY, rightX);
         
         LBPos=Rotation.get_position();
